db449a86812a9fc514d2e0fbb4c5bc29547134f3
vannWithF <vannwithf.jees@gmail.com>
2024-11-24 23:10:54 +0700

    create design for profiles

---
+++ pages/profiles/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex h-screen bg-tertiary outline outline-primary rounded-2xl">
+        <div class="flex h-24 w-24 mx-6 my-8 bg-white rounded-full outline-1 outline outline-primary">
+            <span class="absolute mx-16 mt-16 h-9 w-9 bg-[#6E9B65] rounded-full "></span>
+        </div>
+        <span class="flex ml-6 mt-12">
+            <h1 class="text-3xl font-semibold">JokoGaming</h1>
+            <span class="absolute mt-8">
+                <h2 class="font-semibold text-lg">Joko Wimulyono</h2>
+            </span>
+        </span>
+
+    </div>
+</template>
